7362f2e4a2bc9cdf28c7ddd06c5428e0181cef2c
Mate Farkas <mate.farkas@sch.hu>
2017-09-26 18:14:27 +0100

    Rules have been added.

--- src/main/java/com/masabi/sonar/plugin/pdk/languages/PdkQualityProfile.java
+++ src/main/java/com/masabi/sonar/plugin/pdk/languages/PdkQualityProfile.java
@@ -22,6 +22,8 @@ package com.masabi.sonar.plugin.pdk.languages;
 
 import static com.masabi.sonar.plugin.pdk.rules.PdkLintRulesDefinition.REPO_KEY;
 
+import static org.sonar.api.rules.RulePriority.BLOCKER;
+import static org.sonar.api.rules.RulePriority.INFO;
 import static org.sonar.api.rules.RulePriority.MAJOR;
 import static org.sonar.api.rules.RulePriority.MINOR;
 
@@ -40,8 +42,22 @@ public final class PdkQualityProfile extends ProfileDefinition {
     RulesProfile profile = RulesProfile.create("PDK Rules", PdkLanguage.KEY);
 
     profile.activateRule(Rule.create(REPO_KEY, "pdk:generic"), MAJOR);
+
+    profile.activateRule(Rule.create(REPO_KEY, "metadata-json-lint:error"), MAJOR);
+    profile.activateRule(Rule.create(REPO_KEY, "metadata-json-lint:warning"), MINOR);
+
+    profile.activateRule(Rule.create(REPO_KEY, "metadata-syntax:error"), MAJOR);
+
     profile.activateRule(Rule.create(REPO_KEY, "puppet-lint:error"), MAJOR);
     profile.activateRule(Rule.create(REPO_KEY, "puppet-lint:warning"), MINOR);
+
+    profile.activateRule(Rule.create(REPO_KEY, "puppet-syntax:error"), BLOCKER);
+
+    profile.activateRule(Rule.create(REPO_KEY, "rubocop:convention"), MINOR);
+    profile.activateRule(Rule.create(REPO_KEY, "rubocop:error"), BLOCKER);
+    profile.activateRule(Rule.create(REPO_KEY, "rubocop:fatal"), BLOCKER);
+    profile.activateRule(Rule.create(REPO_KEY, "rubocop:refactor"), INFO);
+    profile.activateRule(Rule.create(REPO_KEY, "rubocop:warning"), MAJOR);
 
     return profile;
   }
